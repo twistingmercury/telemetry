b2dc6ef4077741b8f52f9283f3ae15bb11594e42
Jeremy Johnson <jeremy.johnson@mcg.com>
2024-05-29 14:07:01 -0700

    corrected metrics example

--- _example/metrics/main.go
+++ _example/metrics/main.go
@@ -1,74 +1,39 @@
 package main
 
 import (
-	"context"
-	"log"
-	"math/rand"
+	"bufio"
+	"example/metrics/data"
+	"fmt"
+	"os"
 	"time"
 
-	"github.com/twistingmercury/telemetry/attributes"
 	"github.com/twistingmercury/telemetry/metrics"
-	"go.opentelemetry.io/otel/attribute"
-	"go.opentelemetry.io/otel/exporters/stdout/stdoutmetric"
-	otelmetric "go.opentelemetry.io/otel/metric"
 )
 
 func main() {
-	// Create attributes attributes
-	attribs := attributes.NewWithBatchDuration(
-		"namespace",
-		"service",
-		"1.0.0",
-		"production",
-		1*time.Second, // set very short batching duration for demonstration purposes
-		attribute.String("custom_key", "custom_value"))
+	// Metrics are hosted on in a separate goroutine on the port specified.
+	// This needs to be invoked before any metrics are registered. It can be called
+	// multiple times, but only the first call will have any effect.
+	metrics.Initialize("9090", "metrics", "example")
 
-	// Create an OpenTelemetry stdout exporter
-	exporter, err := stdoutmetric.New()
-	if err != nil {
-		log.Fatal("failed to create stdout exporter:", err)
-	}
+	// Register the metrics from any packages that have them, in this example,
+	// the data package has metrics.
+	dataMetrics := data.Metrics()
 
-	// Initialize the Metrics package with the exporter and common attributes
-	err = metrics.Initialize(exporter, attribs)
-	if err != nil {
-		log.Fatal("failed to initialize metrics package:", err)
-	}
+	// this can be called multiple times if there are metrics in multiple packages.
+	metrics.RegisterCustomMetrics(dataMetrics...)
 
-	// Get the meter instance
-	meter := metrics.Meter()
+	// Publish exposes the metrics for scraping. This needs to be called after
+	// all metrics have been registered. It can be called multiple times, but
+	// only the first call will have any effect.
+	metrics.Publish()
 
-	// Create a new int64 counter metric
-	counter, err := meter.Int64Counter(
-		"example_counter",
-		otelmetric.WithDescription("An example counter metric"))
-	if err != nil {
-		log.Fatal("failed to create counter metric: ", err)
-	}
-
-	// Create a new float64 histogram metric
-	histogram, err := meter.Float64Histogram(
-		"example_histogram",
-		otelmetric.WithDescription("An example histogram metric"))
-	if err != nil {
-		log.Fatal("failed to create histogram metric: ", err)
-	}
-	// Create a context with a timeout
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cancel()
-
-	// Simulate some work and record metrics
 	for i := 0; i < 5; i++ {
-		// Simulate some work
-		time.Sleep(1 * time.Second)
-
-		// Record a value for the counter metric
-		counter.Add(ctx, 1)
-
-		// Record a value for the histogram metric
-		value := rand.Float64() * 100
-		histogram.Record(ctx, value)
-
-		log.Printf("Recorded metrics - Counter: 1, Histogram: %.2f", value)
+		_ = data.DoDatabaseStuff()
+		time.Sleep(2 * time.Second)
 	}
+
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("press any key to quit> ")
+	_, _ = reader.ReadString('\n')
 }

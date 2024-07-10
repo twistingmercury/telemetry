8c20ce84fe5ea117d94cb0549e197ef18026dc7c
Jeremy Johnson <jeremy.johnson@mcg.com>
2024-07-10 16:04:40 -0700

    update module path, updated dependencies

--- metrics/metrics_test.go
+++ metrics/metrics_test.go
@@ -5,7 +5,7 @@ import (
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
-	"github.com/twistingmercury/telemetry/metrics"
+	"github.com/twistingmercury/telemetry/v2/metrics"
 	"testing"
 	"time"
 )
